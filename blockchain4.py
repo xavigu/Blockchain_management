51c143de646e6706a843eaf35f037b6850d6349b
xavigu <xavigu91@gmail.com>
2021-01-03 18:43:48 +0100

    Logic to add a set to the participants of the transaction

--- blockchain4.py
+++ blockchain4.py
@@ -2,6 +2,8 @@ genesis_block = {'previous_hash': '', 'index': 0, 'transactions': []}
 blockchain = [genesis_block]
 open_transactions = []
 owner = 'Javier'
+# create a set to the participants of the transactions
+participants = {'Javier'}
 
 # Extraer el ultimo valor que tiene el blockchain
 # En una lista utilizando -1 accedes al valor que este más a la izquierda de la lista/array y no da error si esta vacio 
@@ -21,6 +23,9 @@ def add_transaction(recipient, sender=owner, amount=1.0):
     """
     transaction = {'sender': sender, 'recipient': recipient, 'amount': amount}
     open_transactions.append(transaction)
+    # Add the participants into the set (automatically ignore a value if is duplicated)
+    participants.add(sender)
+    participants.add(recipient)
 
 # ----------------------------------------------------
 # function to create hashed block
@@ -60,6 +65,11 @@ def print_blockchain_blocks():
         print('Outputting block')
         print(block)
 
+def print_participants():
+    print('Participants list: ', participants)
+
+
+
 # ----------------------------------------------------
 # Comprueba que el primer valor de la segunda blockchain coincide con el valor de la blockchain previa
 def verify_chain():
@@ -82,6 +92,7 @@ while waiting_for_input:
     print('1: Add a new transaction value')
     print('2: Output the blockchain blocks')
     print('3: Mine a new block')
+    print('4: Output the participants')
     print('e: Manipulate a blockchain block')
     print('q: Finish the transactions')
     user_choice = get_user_choice()
@@ -94,6 +105,8 @@ while waiting_for_input:
         print_blockchain_blocks()
     elif user_choice == '3':
         mine_block()
+    elif user_choice == '4':
+        print_participants()
     elif user_choice == 'e':
         if len(blockchain) >= 1:
             blockchain[0] = {'previous_hash': '', 'index': 0, 'transactions': [{'sender':'Chris', 'recipient':'Tom', 'amount':10.5}]}
